5d393f27f8b31ba2ae4b856caf526fe0817533fb
Bruno Eduardo dos Santos <10363094@mydbs.ie>
2020-02-20 19:01:34 +0000

    adding new functions.

--- collegium/collegium/GeneralTools.cs
+++ collegium/collegium/GeneralTools.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,6 +19,19 @@ namespace collegium
             MessageBox.Show(message, caption, MessageBoxButtons.OK);
         }
 
+        public bool IsValid(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
 
 
     }
